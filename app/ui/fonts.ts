303f55616cd27676f0602c396e6edc0aed285c7f
vadflor <vadflor@gmail.com>
2024-02-17 20:03:52 +0200

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Inter({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
